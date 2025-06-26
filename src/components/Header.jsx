4aecb167817ae26bc5c3d7d50baf247ae76769e8
dharunDP <dharunpranav.ad22@bitsathy.ac.in>
2025-06-26 20:05:06 +0530

    Final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <img src="dplogo(1).png" alt="Profile" className="profile-img" />
+      <img src="dplogo2025.png" alt="Profile" className="profile-img" />
       <h1>DHARUN PRANAV S K</h1>
     </div>
   );
